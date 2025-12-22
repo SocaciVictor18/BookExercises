8662546fd589e228247f8a72ea6f11696fb6515e
Victor Socaci <Victor.Socaci@endava.com>
2025-12-22 15:35:48 +0200

    add blueprint of the project

---
+++ BookExercises/src/test/java/org/example/bookexercises/BookExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
